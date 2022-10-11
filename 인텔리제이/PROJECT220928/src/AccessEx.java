d1cc95cf54302bc557eced22b3eda09aa52f2234
joeunji01 <eunzs@naver.com>
2022-10-11 21:06:26 +0900

    commit change

---
+++ 인텔리제이/PROJECT220928/src/AccessEx.java
@@ -0,0 +1,13 @@
+//class Sample{
+//    public int a;
+//    private int b;
+//    int c;
+//}
+//public class AccessEx {
+//    public static void main(String[] args){
+//        Sample aClass = new Sample();
+//        aClass.a=10;
+//        aClass.b=10; //b has private access in Sample
+//        aClass.c=10;
+//    }
+//}
---
+++ 인텔리제이/PROJECT220928/src/Circle.java
@@ -0,0 +1,25 @@
+//public class Circle {
+//    int radius;
+//    String name;
+//    public void Circle(){} //생성자
+//    public double getArea(){
+//        return 3.14*radius*radius;
+//    }
+//
+//    public static void main(String[] args){
+//        Circle pizza; //객체 생성
+//        pizza=new Circle();
+//        pizza.radius=10;
+//        pizza.name ="자바피자";
+//        double area=pizza.getArea();
+//        System.out.println(pizza.name+"의 면적은 "+area);
+//
+//        Circle donut =new Circle(); //객체 생성
+//        donut.radius=2;
+//        donut.name="자바도넛";
+//        area=donut.getArea();
+//        System.out.println(donut.name+"의 면적은 "+area);
+//
+//
+//    }
+//}
